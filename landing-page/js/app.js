00519b06e68c1e9a364fc341070694cbfa59833e
Sara Hamam <66142040+Sarahamam@users.noreply.github.com>
2020-11-22 21:12:08 +0400

    Update app.js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -17,6 +17,12 @@
  * Define Global Variables
  * 
 */
+//defining a global variable called (sections) 
+const sections = document.querySelectorAll("section");
+//defining a global variable called (navigation) 
+const navigation = document.getElementByID('navbar__list'); 
+//let navbarList = document.querySelector("#navbar__list");
+
 
 
 /**
@@ -24,7 +30,16 @@
  * Start Helper Functions
  * 
 */
+function isInViewport(elem) {
+	var distance = elem.getBoundingClientRect();
 
+	return (
+		distance.top >= -300 &&
+		distance.left >= 0 &&
+		distance.bottom <= (1.3 * window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
 
 
 /**
@@ -34,13 +49,38 @@
 */
 
 // build the nav
-
+window.addEventListener('load', buildNavbar())
 
 // Add class 'active' to section when near top of viewport
+function activateCurrentSection(currentSection) {
+    currentSection.classList.add("your-active-class", "active");
 
+    deactivateNavLinks();
+    activateNavLinks(currentSection.getAttribute('id'));
+}
+
+function activateNavLinks(currentSectionId) {
+    let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
+    //console.log(navbarAnchors);
+        navbarAnchors.forEach((element)=>{
+            if(element.getAttribute('href') == `#${currentSectionId}`) {
+                element.classList.add("active-nav");
+            }
+        });
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSectionOnClick() {
+    let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
+    navbarAnchors.forEach((element) => {
+        element.addEventListener("click", function(event) {
+            event.preventDefault();
+            document.querySelector(element.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
 
 /**
  * End Main Functions
@@ -49,9 +89,34 @@
 */
 
 // Build menu 
+function buildNavbar() {
+	sections.forEach((element)=>{
+	    let listItem = document.createElement("li");
+	    listItem.classList.add("navbar__list__item");
+    	let sectionName = element.getAttribute("data-nav");
+    	let currentSectionId = element.getAttribute("id");
+        listItem.innerHTML = `<a href="#${currentSectionId}" class="nav__hyperlink">${sectionName}</a>`;
+        navbarList.appendChild(listItem);
+    });
+}
 
 // Scroll to section on link click
-
+scrollToSectionOnClick();
 // Set sections as active
-
+window.addEventListener('scroll', function (event) {
+	event.preventDefault();
+	
+    sections.forEach((element) => {
+        // console.log(element);
+        if (isInViewport(element)) {
+            deactivateSections();
+            activateCurrentSection(element);
+            // console.log('In viewport!');
+        } else if(window.scrollY==0) {
+            deactivateSections();
+            deactivateNavLinks();
+            // console.log('No Change');
+        }
+    }, false);
+});
 
