a742a7fbe7417597b2b96f652fa974672c73c5b9
Sara Hamam <66142040+Sarahamam@users.noreply.github.com>
2020-12-04 17:38:00 +0400

    Update app.js

--- Weather Journal App/website/app.js
+++ Weather Journal App/website/app.js
@@ -3,3 +3,86 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '3aa184cc6c3bddad7b8e707ae1f8aafe';
+
+let today = new Date().toDateString();
+
+document.getElementById('generate').addEventListener('click', generateData);
+
+const zipError = document.querySelector('.zip-error');
+
+
+function generateData(event) {
+    zipError.style.display === 'none';
+    zipError.innerText = '';
+    const zipCode = document.getElementById('zip').value;
+    if (!zipCode) {
+        zipError.style.display === 'none';
+        zipError.innerText = "Please enter zip.";
+    }
+    const targetURL = baseURL + zipCode + '&appid=' + apiKey + '&units=imperial';
+    const content = document.getElementById('feelings').value;
+    retrieveData(targetURL)
+        .then(function (jsonData) {
+            postData('/addData', { date: today, temp: jsonData.main.temp, content: content, city: jsonData.name });
+            return { date: today, temp: jsonData.main.temp, content: content, city: jsonData.name };
+        })
+        .then(function () {
+            updateUI()
+        }
+        )
+}
+
+// Return JSON data from the exernal API
+const retrieveData = async (url) => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const jsonData = await request.json();
+        if (jsonData.cod === "404") {
+            zipError.style.display = 'block';
+            zipError.innerText = "No data on server for this zip. Please enter another zip.";
+            return;
+        }
+        return jsonData;
+    }
+    catch (error) {
+        console.log("error", error);
+    }
+}
+
+// Add data to the project endpoint using POST
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+// Update UI dynamically
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById('entry-header').style.display = 'none';
+        document.getElementById('date').innerText = allData.date;
+        document.getElementById('temp').innerHTML = `${allData.temp} &deg;F in ${allData.city}`;
+        document.getElementById('content').innerText = allData.content;
+        document.getElementById('zip').value = '';
+        document.getElementById('feelings').value = '';
+    } catch (error) {
+        console.log("error", error);
+    }
+}
