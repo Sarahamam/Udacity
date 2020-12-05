0212b9b1a5226659687ff51cabf45181844107ea
Sara Hamam <66142040+Sarahamam@users.noreply.github.com>
2020-12-05 22:12:06 +0400

    Add files via upload

--- Weather Journal App/commentsOnlyJS/app.js
+++ Weather Journal App/commentsOnlyJS/app.js
@@ -1,12 +1,63 @@
-// Personal API Key for OpenWeatherMap API
+/* Global Variables */
+const form = document.querySelector('.app__form');
+const icons = document.querySelectorAll('.entry__icon');
+const appID = '8fe35d267fec84174d32e7bac0493cea';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
+const zipInput = document.getElementById('zip');
+const userInput = document.getElementById('feelings')
+const dateHolder = document.getElementById('date')
+const tempHolder = document.getElementById('temp')
+const contentHolder = document.getElementById('content')
+const postURL = 'http://localhost:8000'
+const getURL = 'http://localhost:8000/all'
 
-// Event listener to add function to existing HTML DOM element
+// Dynamically create a new JS date instance
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
-/* Function called by event listener */
+// Call function to fetch via OpenWeatherMap
+const getWeather = async (baseURL, zip = '94712,us', api) => {
+  const url = `http://${baseURL}zip=${zip}&appID=${api}`
+  const response = await fetch(url)
+  let jsonResponse = await response.json()
+  return jsonResponse
+}
 
-/* Function to GET Web API Data*/
+// User-input post data function
+const postData = async (path, data = {}) => {
+  const response = await fetch(path, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow', // manual, *follow, error
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+}
 
-/* Function to POST data */
+// Update UI function 
+const updateUI = async () => {
+  const response = await fetch(getURL)
+  const jsonResponse = await response.json()
+  dateHolder.innerHTML = `<span class="entry-item">Date: </span>${jsonResponse.date}`
+  contentHolder.innerHTML = `<span class="entry-item">You feel: </span>${jsonResponse.userResponse}`
+  tempHolder.innerHTML = `<span class="entry-item">Temperature: </span>${jsonResponse.temperature}`
+}
 
+// Event handler handleClick
+const handleClick = async () => {
+  const weatherData = await getWeather(baseURL, zipInput.value, appID)
+  const data = {
+    temperature: weatherData.main.temp,
+    date: newDate,
+    userresponse: userInput.value
+  }
+  await postData(postURL, data)
+  updateUI()
+}
 
-/* Function to GET Project Data */
+// Add element event listener with 'generate' id
+const ele = document.getElementById('generate')
+ele.addEventListener('click', handleClick)
