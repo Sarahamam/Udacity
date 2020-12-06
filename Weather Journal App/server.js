cadb4236f8562d34f08bef0a76ff7c19317d767c
Sara Hamam <66142040+Sarahamam@users.noreply.github.com>
2020-12-06 11:14:17 +0400

    Update server.js

--- Weather Journal App/server.js
+++ Weather Journal App/server.js
@@ -1,36 +1,29 @@
-// Setup empty JS object to act as endpoint for all routes
-let projectData = {};
+// Initialize global vars
+const projectData = []; // Empty array used as endpoint for all routes
+const port = 8080;
 
-// Require Express to run server and routes
-const express = require('express');
+// Include Node.js modules
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const cors = require( 'cors' );
 
-// Dependencies
-const bodyParser = require('body-parser');
-
-// Start up an instance of app
+// Initialize instance of the server using Express
 const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Setup server
+app.use( express.static('www') ); // Specify app directory
+app.use( bodyParser.urlencoded({ extended: false }) );
+app.use( bodyParser.json() );
+app.use( cors() ); // Allow cross origin functionality
+app.listen( port, portInfo );
 
-// Cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
-// Initialize the main project folder
-app.use(express.static('website'));
+// Port info callback
+function portInfo(){
+	console.log( `Server Running on Port: ${port}` );
+}
 
-// Post Route
 // Add POST route
 app.post( '/upload', postData );
-
-function addInfo(req, res) {
-  projectData['date'] = req.body.date;
-  projectData['temp'] = req.body.temp;
-  projectData['content'] = req.body.content;
-  res.send(projectData);
-}
 
 // Function that handles POST requests
 function postData( request, response ){
@@ -39,6 +32,8 @@ function postData( request, response ){
 	console.log( 'postData()' );
 	console.log( request.body );
 	response.send( request.body );
+
+}
 
 // Add GET route
 app.get( '/all', getData );
@@ -50,25 +45,4 @@ function getData( request, response ){
 	console.log( projectData );
 	response.send( projectData );
 
-}
-// Setup Server
-/*
-const port = 8000;
-const server = app.listen(port, listening);
-
-function listening() {
-  console.log(`running on localhost: ${port}`);
-};
-*/
-// Setup server
-const port = 8000;
-app.use( express.static('www') ); // Specify app directory
-app.use( bodyParser.urlencoded({ extended: false }) );
-app.use( bodyParser.json() );
-app.use( cors() ); // Allow cross origin functionality
-app.listen( port, portInfo );
-
-// Port info callback
-function portInfo(){
-	console.log( `Server Running on Port: ${port}` );
 }
