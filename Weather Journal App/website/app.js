cf6fca271ba96b20e3bc69a2358db2493e7b6505
Sara Hamam <66142040+Sarahamam@users.noreply.github.com>
2020-12-06 09:22:13 +0400

    Update app.js

--- Weather Journal App/website/app.js
+++ Weather Journal App/website/app.js
@@ -1,6 +1,6 @@
 
 /* Global Project Variables */
-
+/*
 const baseURL = 'api.openweathermap.org/data/2.5/weather?'
 const appid = '8fe35d267fec84174d32e7bac0493cea';
 const zipInput = document.getElementById('zip');
@@ -61,3 +61,93 @@ const handleClick = async () => {
 // Add element event listener with 'generate' id
 const ele = document.getElementById('generate')
 ele.addEventListener('click', handleClick)
+*/
+
+---------------------
+  /* Global Variables */
+const form = document.querySelector('.app__form');
+const icons = document.querySelectorAll('.entry__icon');
+
+// Base URL and API Key for OpenWeatherMap API
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=8fe35d267fec84174d32e7bac0493cea';
+
+//Get the date
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e) {
+  e.preventDefault();
+  // get user input values
+  const newZip = document.getElementById('zip').value;
+  const content = document.getElementById('feelings').value;
+
+  getWeather(baseURL, newZip, apiKey)
+    .then(function (userData) {
+      // add data to POST request
+      postData('/add', { date: newDate, temp: userData.main.temp, content })
+    }).then(function (newData) {
+      // call updateUI to update browser content
+      updateUI()
+    })
+  // reset form
+  form.reset();
+}
+
+/* Function to GET Web API Data*/
+const getWeather = async (baseURL, newZip, apiKey) => {
+  // res equals to the result of fetch function
+  const res = await fetch(baseURL + newZip + apiKey);
+  try {
+    // userData equals to the result of fetch function
+    const userData = await res.json();
+    return userData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  const req = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8"
+    },
+    body: JSON.stringify({
+      date: data.date,
+      temp: data.temp,
+      content: data.content
+    })
+  })
+
+  try {
+    const newData = await req.json();
+    return newData;
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json()
+    // show icons on the page
+    icons.forEach(icon => icon.style.opacity = '1');
+    // update new entry values
+    document.getElementById('date').innerHTML = allData.date;
+    document.getElementById('temp').innerHTML = allData.temp;
+    document.getElementById('content').innerHTML = allData.content;
+  }
+  catch (error) {
+    console.log("error", error);
+  }
+};
