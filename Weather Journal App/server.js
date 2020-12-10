4c5a866313ffef1c1a524fe74fc5c38c8b7c0782
Sara Hamam <66142040+Sarahamam@users.noreply.github.com>
2020-12-11 01:03:13 +0400

    Update server.js

--- Weather Journal App/server.js
+++ Weather Journal App/server.js
@@ -23,12 +23,19 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-app.post('/add', function (req, res) {
-  projectData = req.body;
+// GET route
+app.get('/data', (req, res) => {
+  console.log('GET request received');
+  res.send(projectData);
 });
 
-app.get('/all', function (req, res) {
-    res.send(projectData);
+// POST route
+app.post('/', (req, res) => {
+  projectData.date = req.body.date;
+  projectData.temperature = req.body.main.temp;
+  projectData.feelings = req.body.feelings;
+  console.log('POST request received');
+  res.end();
 });
 
 // Setup Server
